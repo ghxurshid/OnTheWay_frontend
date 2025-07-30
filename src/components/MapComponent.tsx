b8ed998b9c4883fb6977908b3dfd6ff087ffea6b
Xurshid <khurshid.khujamatov@mail.ru>
2025-07-30 12:48:39 +0500

    Routing with polyline

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -8,6 +8,7 @@ const MapComponent = () => {
   const [map, _setMap] = useState(null);
   const currentLoc = useRef<L.Marker<any> | null>(null);
   const destineLoc = useRef<L.Marker<any> | null>(null); 
+  const router = L.Routing.osrmv1(); // default OSRM router
 
   useEffect(() => {
     const mapInstance = map ? map : L.map('map', {
@@ -18,31 +19,73 @@ const MapComponent = () => {
     });
      
     currentLoc.current = L.marker([41.3273629, 69.330082]).addTo(mapInstance);
-    /*var _circle = */L.circle([41.3273628, 69.330082], {
-      color: 'green',
-      fillColor: '#f03',
-      fillOpacity: 0.2,
-      radius: 50
-    }).addTo(mapInstance);
+    
+    // var _circle = L.circle([41.3273628, 69.330082], {
+    //   color: 'green',
+    //   fillColor: '#f03',
+    //   fillOpacity: 0.2,
+    //   radius: 50
+    // }).addTo(mapInstance);
 
-    /*var _polygon = */L.polygon([
-      [41.327624, 69.329116],
-      [41.326045, 69.326895],
-      [41.325856, 69.331133]
-    ]).addTo(mapInstance);
+    // var _polygon = L.polygon([
+    //   [41.327624, 69.329116],
+    //   [41.326045, 69.326895],
+    //   [41.325856, 69.331133]
+    // ]).addTo(mapInstance);
 
     const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    L.Routing.control({
-      waypoints: [
-        L.latLng(41.311081, 69.240562), // Boshlanish nuqta
-        L.latLng(41.2995, 69.2401)      // Tugash nuqta
-      ],
-      routeWhileDragging: true
-    }).addTo(mapInstance);
+    /*
+    // var control = L.Routing.control({
+    //   waypoints: [
+    //     L.latLng(41.327840, 69.331134), // Boshlanish nuqta
+    //     L.latLng(41.268693, 69.292682)      // Tugash nuqta
+    //   ],
+    //   routeWhileDragging: true,
+          
+    // }).addTo(mapInstance);
+ 
+    // // Cache tiles for offline use// Hodisa bilan yo'l topilganda chaqiriladi
+    // control.on('routesfound', function (e) {
+    //   const routes = e.routes;
 
+    //   if (routes.length > 0) {
+    //     const coordinates = routes[0].coordinates;
+
+    //     console.log('Koordinatalar:', coordinates); // 👈 Barcha koordinatalar (LatLng[])
+
+    //     // Agar istasangiz har birini alohida chiqarsangiz ham bo‘ladi
+    //     // coordinates.forEach((coord: L.LatLng, index: number) => {
+    //     //   console.log(`Nuqta ${index + 1}: ${coord.lat}, ${coord.lng}`);
+    //     // });
+    //   }
+    // });
+    */
+   
+    const waypoints = [
+      new L.Routing.Waypoint(L.latLng(41.311081, 69.240562), '', {}), // 'name' bo‘sh string
+      new L.Routing.Waypoint(L.latLng(41.2995, 69.2401), '', {})
+    ];
+ 
+    // router.route(
+    //   waypoints,
+    //   ((err: any, routes: L.Routing.IRoute[]) => {
+    //     if (err) {
+    //       console.error("Xato:", err);
+    //       return;
+    //     } 
+         
+    //     if (routes?.length) {
+    //       const route = routes[0];
+    //       route?.coordinates?.forEach((coord, i) => {
+    //         console.log(`Step ${i + 1}: ${coord.lat}, ${coord.lng}`);
+    //       });
+    //     }
+    //   }) as unknown as (args?: any) => void // ✅ bu yerda TypeScriptni “aldayapmiz”
+    // );
+        
     tileLayer.on('tileload', (event) => {
       const url = event.tile.src;
       caches.open('map-cache-v1').then((cache) => {
@@ -53,38 +96,46 @@ const MapComponent = () => {
     tileLayer.addTo(mapInstance);
 
     const latlngs: L.LatLngTuple[] = [
-      [41.3273628, 69.330082],
-      [41.327624, 69.329116],
-      [41.326045, 69.326895],
-      [41.325856, 69.331133]
+      // [41.3273628, 69.330082],
+      // [41.327624, 69.329116],
+      // [41.326045, 69.326895],
+      // [41.325856, 69.331133]
     ];
+
     var polyline = L.polyline(latlngs, {color: 'green', opacity: 0.7, weight: 9}).addTo(mapInstance);
 
-    mapInstance.on('click', (e) => {    
-      polyline.addLatLng(e.latlng);
-      mapInstance.fitBounds(polyline.getBounds());
-
+    mapInstance.on('click', (e) => {
       if (!destineLoc.current) {
         destineLoc.current = L.marker(e.latlng).addTo(mapInstance);
       } else {
         destineLoc.current.setLatLng(e.latlng);
       }
+      
+      if (currentLoc &&  destineLoc) { 
+        const waypoints = [
+          new L.Routing.Waypoint(currentLoc.current.getLatLng(), '', {}), // 'name' bo‘sh string
+          new L.Routing.Waypoint(destineLoc.current.getLatLng(), '', {})
+        ];
+ 
+        // Routingni yangilash
+        router.route(waypoints, (err: any, routes: L.Routing.IRoute[]) => {
+          if (err) {
+            console.error("Xato:", err);
+            return;
+          }
+          
+          if (routes?.length) {
+            const route = routes[0];
 
-      //Строим маршрут от текущего местоположения до выбранной точки
-      // L.Routing.control({
-      //   waypoints: [
-      //     L.latLng(currentLoc.current),
-      //     L.latLng(destineLoc.current)
-      //   ],
-      //   router: L.routing.osrmv1({
-      //     serviceUrl: 'https://router.project-osrm.org/route/v1'
-      //   }),        
-      //   routeWhileDragging: true,
-      // }).addTo(mapInstance);      
+            console.log('Route coordinates:', route.coordinates);
+
+            polyline.setLatLngs(route.coordinates.map(coord => [coord.lat, coord.lng]));
+            mapInstance.fitBounds(polyline.getBounds());
+          }
+        }) as unknown as (args?: any) => void // ✅ bu yerda TypeScriptni “aldayapmiz”;
+      }      
     });
-
-    // setMap(mapInstance);
-
+ 
     // Функция для отслеживания местоположения
     function startTracking() {
       if (!navigator.geolocation) {
@@ -96,15 +147,13 @@ const MapComponent = () => {
         (position) => {
           const { latitude, longitude } = position.coords;
           const userLocation: [number, number] = [latitude, longitude];
-    
-          console.info('User location:', userLocation);
-    
+   
           // Har doim xaritani markazga olib borish
           //mapInstance.setView(userLocation, 13);
     
           // Marker mavjud bo‘lsa, uni yangilash
           if (currentLoc.current) {
-            currentLoc.current.setLatLng(userLocation);
+            currentLoc.current.setLatLng(userLocation).bindPopup('You are here!').openPopup();
           } else {
             // Marker yo‘q bo‘lsa, yangi marker yaratish
             currentLoc.current = L.marker(userLocation)
@@ -126,7 +175,7 @@ const MapComponent = () => {
 
     // Начинаем отслеживание
     startTracking();
-
+ 
     // Очистка ресурсов при размонтировании компонента
     return () => {
       
